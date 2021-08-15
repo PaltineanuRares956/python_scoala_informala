ff877c0ee9de885cff0b502dcb735c999ca5c54d
PaltineanuRares956 <paltineanu_rares@yahoo.com>
2021-08-15 15:50:51 +0300

    1.0.7

--- final_project/final_project/final_project/settings.py
+++ final_project/final_project/final_project/settings.py
@@ -100,7 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
     #     # 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     # },
 ]
-
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
 AUTH_USER_MODEL = 'users.User'
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
--- final_project/final_project/products/views.py
+++ final_project/final_project/products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from .models import Product
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+
+def products_view(request):
+
+    if request.POST:
+        print('123')
+        if 'Products' in request.POST:
+            print('12')
+            return HttpResponseRedirect(reverse('products-list'))
+
+    return render(request, 'products.html', {})
+
+
+def products_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'queryset': queryset
+    }
+    return render(request, 'products-list.html', context)
