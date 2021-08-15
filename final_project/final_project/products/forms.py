b6ac12b2314ce1f8f14bffec5c6aa6af6738d0c7
PaltineanuRares956 <paltineanu_rares@yahoo.com>
2021-08-15 22:52:39 +0300

    final_project 1.0.9

--- final_project/final_project/products/forms.py
+++ final_project/final_project/products/forms.py
@@ -1,5 +1,21 @@
 from django import forms
 
+FILTER_CHOICES = [
+    ('protein', 'Proteins'),
+    ('fat', 'Fats'),
+    ('carbohydrate', 'Carbohydrates')
+]
+
 
 class FilterProductForm(forms.Form):
-    pass
+    min_value = forms.DecimalField(decimal_places=2,
+                                   max_digits=4,
+                                   min_value=0,
+                                   max_value=100,
+                                   required=False)
+    max_value = forms.DecimalField(decimal_places=2,
+                                   max_digits=4,
+                                   min_value=0,
+                                   max_value=100,
+                                   required=False)
+    filter_choice = forms.CharField(widget=forms.RadioSelect(choices=FILTER_CHOICES))
--- final_project/final_project/products/views.py
+++ final_project/final_project/products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, reverse
 from .models import Product
 from django.http import HttpResponseRedirect
-
+from .forms import FilterProductForm
 # Create your views here.
 
 
@@ -17,21 +17,42 @@ def products_view(request):
 
 
 def products_list_view(request):
+    form = FilterProductForm()
     queryset = Product.objects.all()
-    context = {}
-    if request.GET:
-        print('123')
-        print(request.GET)
-        if 'Min' and 'Max' in request.GET:
-            min_value = request.GET.get('Min')
-            max_value = request.GET.get('Max')
+    context = {
+        'form': form,
+        'queryset': queryset
+    }
 
-            if 'Proteins' in request.GET:
-                context['proteins'] = True
-            if 'Fats' in request.GET:
-                context['fats'] = True
-            if 'Carbohydrates' in request.GET:
-                context['carbohydrates'] = True
+    if request.GET:
+        #if request.GET.get('min_value') != '' and request.GET.get('max_value') != '':
+        if 'All' not in request.GET:
+            min_value = request.GET.get('min_value')
+            max_value = request.GET.get('max_value')
+            filter_choice = request.GET.get('filter_choice')
+            if min_value > max_value:
+                context['invalid'] = True
+            else:
+                if filter_choice == 'protein':
+                    queryset = Product.objects.filter(proteins__gte=min_value, proteins__lte=max_value)
+                elif filter_choice == 'fat':
+                    queryset = Product.objects.filter(fats__gte=min_value, fats__lte=max_value)
+                elif filter_choice == 'carbohydrate':
+                    queryset = Product.objects.filter(carbohydrates__gte=min_value, carbohydrates__lte=max_value)
+                context['queryset'] = queryset
+    # if request.GET:
+    #    print('123')
+    #    print(request.GET)
+    #    if 'Min' and 'Max' in request.GET:
+    #        min_value = request.GET.get('Min')
+    #        max_value = request.GET.get('Max')
+#
+    #        if 'Proteins' in request.GET:
+    #            context['proteins'] = True
+    #        if 'Fats' in request.GET:
+    #            context['fats'] = True
+    #        if 'Carbohydrates' in request.GET:
+    #            context['carbohydrates'] = True
 
 
     return render(request, 'products-list.html', context)
